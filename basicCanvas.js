e2efed5bf3ba6b52eef13772453732236cf854b5
minju0809 <loverminju88@naver.com>
2022-12-12 20:28:21 +0900

    wildlife_three.js

---
+++ basicCanvas.js
@@ -0,0 +1,58 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const canvas = document.querySelector("#canvas");
+const meshes = [];
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const light_1 = new THREE.PointLight();
+scene.add(light_1);
+light_1.position.set(0, 0, 30);
+const light_2 = new THREE.PointLight();
+scene.add(light_2);
+light_2.position.set(0, 0, -30);
+
+const controls = new OrbitControls(camera, canvas);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+for (let i = 0; i < 500; i++) {
+  const cube = createCube();
+  meshes.push(cube);
+  scene.add(cube);
+
+  cube.position.set(Math.random() * 20 - 10, Math.random() * 20 - 10, 0);
+}
+
+camera.position.z = 30;
+
+function animate(time) {
+  requestAnimationFrame(animate);
+
+  meshes.forEach((mesh, index) => {
+    const radius = 1 + (15 * index) / meshes.length;
+    const speed = index / meshes.length / 1000;
+    mesh.position.set(radius * Math.sin(index + time * speed), radius * Math.cos(index + time * speed), radius * Math.sin(index + 2 * time * speed));
+    mesh.lookAt(0, 0, 0);
+  });
+
+  renderer.render(scene, camera);
+}
+
+function createCube() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshPhongMaterial({
+    color:
+      "#" +
+      Math.floor(Math.random() * 0xffffff)
+        .toString(16)
+        .padStart(6, "0"),
+  });
+  const cube = new THREE.Mesh(geometry, material);
+  return cube;
+}
+
+animate();
---
+++ main.js
@@ -0,0 +1,8 @@
+// const basicCanvasBtn = document.querySelector(".basicCanvasBtn");
+
+// basicCanvasBtn.addEventListener("click", () => {
+//   document.body.classList.toggle("canvas");
+//   basicCanvasBtn.querySelector("span:nth-child(1)").classList.toggle("active");
+//   basicCanvasBtn.querySelector("span:nth-child(2)").classList.toggle("active");
+// });
+

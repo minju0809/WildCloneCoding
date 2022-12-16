01769c87eb6a010bea94a240cc8662cfa66780e2
minju0809 <loverminju88@naver.com>
2022-12-16 22:02:13 +0900

    canvasBrush add reset, eraser

--- canvasBrush.js
+++ canvasBrush.js
@@ -2,24 +2,37 @@ const canvas = document.querySelector(".canvasBrush");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const boardResetBtn = document.querySelector(".boardResetBtn");
 const penBtn = document.querySelector(".penBtn");
 const dotsBtn = document.querySelector(".dotsBtn");
 
 let drawable = false;
 let brushType = "";
 
+boardResetBtn.onclick = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+eraserBtn.onclick = () => {
+  if (!eraserBtn.classList.contains("on")) eraserBtn.classList.add("on");
+  if (dotsBtn.classList.contains("on")) dotsBtn.classList.remove("on");
+  if (penBtn.classList.contains("on")) penBtn.classList.remove("on");
+  brushType = "eraser";
+};
 penBtn.onclick = () => {
   if (!penBtn.classList.contains("on")) penBtn.classList.add("on");
   if (dotsBtn.classList.contains("on")) dotsBtn.classList.remove("on");
+  if (eraserBtn.classList.contains("on")) eraserBtn.classList.remove("on");
   brushType = "pen";
 };
 dotsBtn.onclick = () => {
   if (!dotsBtn.classList.contains("on")) dotsBtn.classList.add("on");
   if (penBtn.classList.contains("on")) penBtn.classList.remove("on");
+  if (eraserBtn.classList.contains("on")) penBtn.classList.remove("on");
   brushType = "dots";
 };
 
 const fnTable = {
+  eraser: (x, y) => new Eraser(x, y),
   pen: (x, y) => new Pen(x, y),
   dots: (x, y) => new Dots(x, y),
 };
@@ -33,6 +46,19 @@ canvas.addEventListener("mousemove", (ev) => {
   // new DotsBtn(ev.x, ev.y);
 });
 
+class Eraser {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.draw();
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(234, 237, 230)";
+    ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
 class Pen {
   constructor(x, y) {
     this.x = x;
@@ -46,7 +72,6 @@ class Pen {
     ctx.fill();
   }
 }
-
 class Dots {
   constructor(x, y) {
     this.x = x;
@@ -57,7 +82,6 @@ class Dots {
     this.radius = Math.random() * 4 + 2;
     this.draw();
   }
-
   draw() {
     this.speedX += (Math.random() * 4 - 2) / 2;
     this.speedY += (Math.random() * 4 - 2) / 2;
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const basicCanvasBtn = document.querySelector(".basicCanvasBtn");
 const basicCanvas = document.querySelector(".basicCanvas");
+const boardResetBtn = document.querySelector(".boardResetBtn");
+const eraserBtn = document.querySelector(".eraserBtn");
 const penBtn = document.querySelector(".penBtn");
 const dotsBtn = document.querySelector(".dotsBtn");
 
 basicCanvasBtn.addEventListener("click", function () {
   basicCanvas.classList.toggle("active");
   canvasBrush.classList.remove("active");
+  boardResetBtn.classList.remove("active");
+  eraserBtn.classList.remove("active");
   penBtn.classList.remove("active");
   dotsBtn.classList.remove("active");
 });
@@ -16,6 +20,8 @@ const canvasBrush = document.querySelector(".canvasBrush");
 canvasBrushBtn.addEventListener("click", function () {
   canvasBrush.classList.toggle("active");
   basicCanvas.classList.remove("active");
+  boardResetBtn.classList.toggle("active");
+  eraserBtn.classList.toggle("active");
   penBtn.classList.toggle("active");
   dotsBtn.classList.toggle("active");
 });
